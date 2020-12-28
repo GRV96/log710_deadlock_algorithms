cf13884f6f100947917cc0882751ed0186f504f0
Guyllaume Rousseau <guyllaume.rousseau.1@ens.etsmtl.ca>
2020-12-28 17:38:57 -0500

    Javadoc for class OutputFileWriter

--- src/OutputFileWriter.java
+++ src/OutputFileWriter.java
@@ -3,17 +3,49 @@ import java.io.FileWriter;
 import java.io.IOException;
 import java.util.Iterator;
 
+/**
+ * This class writes lines of text from a FileContent object in a text file.
+ * The path to the output file is given to the constructor as a parameter.
+ * @author Guyllaume Rousseau
+ */
 public class OutputFileWriter {
 
+	/**
+	 * A String object consisting of only the "\n" character
+	 */
 	private static final String NEW_LINE = "\n";
 
+	/**
+	 * This object represents the file into which the output is written.
+	 */
 	private File outputFile = null;
+
+	/**
+	 * The path to the output file
+	 */
 	private String outputPath = null;
 
+	/**
+	 * The instance created by this constructor can write data in a file whose
+	 * path is parameter outputPath.
+	 * @param outputPath - the path to the output file
+	 * @throws NullPointerException if outputPath is null
+	 */
 	public OutputFileWriter(String outputPath) throws NullPointerException {
 		this.outputPath = outputPath;
+		if (outputPath == null)
+		{
+			throw new NullPointerException(
+					"Parameter outputPath cannot be null.");
+		}
 	}
 
+	/**
+	 * Creates the file meant to contain lines of text. If the file already
+	 * exists, a number is appended to the new file's name to differentiate it.
+	 * @throws IOException if an I/O error occurs when the output file is
+	 * created
+	 */
 	private void createOutputFile() throws IOException {
 		int dotIndex = outputPath.lastIndexOf(FileUtil.PERIOD);
 		String pathAndName = outputPath.substring(0, dotIndex);
@@ -27,6 +59,15 @@ public class OutputFileWriter {
 		}
 	}
 
+	/**
+	 * Records the lines from fileContent in a new text file whose path has
+	 * been given to this class' constructor.
+	 * @param fileContent - an object containing lines of text to write to a
+	 * text file
+	 * @throws IOException if an I/O error occurs or if the file exists but is
+	 * a directory rather than a regular file, does not exist but cannot be
+	 * created or cannot be opened for any other reason
+	 */
 	public void writeToFile(FileContent fileContent) throws IOException {
 		createOutputFile();
 		FileWriter fw = new FileWriter(outputFile);
